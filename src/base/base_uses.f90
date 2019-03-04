f8e32a5a216b3a5c411d1001320f6a8acd96fd86
Patrick Seewald <patrick.seewald@gmail.com>
2019-03-04 16:58:31 +0100

    fprettify commit 54befa129b6eec52225aab2c258de635e57d2aed

--- src/base/base_uses.f90
+++ src/base/base_uses.f90
@@ -1,15 +1,14 @@
 ! Basic use statements and preprocessor macros
 ! should be included in the use statements
 
-  USE base_hooks,                      ONLY: cp__a,&
-                                             cp__b,&
-                                             cp__w,&
-                                             cp__l,&
-                                             cp_abort,&
-                                             cp_warn,&
-                                             timeset,&
-                                             timestop
-
+  USE base_hooks, ONLY: cp__a, &
+                        cp__b, &
+                        cp__w, &
+                        cp__l, &
+                        cp_abort, &
+                        cp_warn, &
+                        timeset, &
+                        timestop
 
 ! Dangerous: Full path can be arbitrarily long and might overflow Fortran line.
 #if !defined(__SHORT_FILE__)
@@ -29,7 +28,7 @@
 
 ! The MARK_USED macro can be used to mark an argument/variable as used.
 ! It is intended to make it possible to switch on -Werror=unused-dummy-argument,
-! but deal elegantly with e.g. library wrapper routines that take arguments only used if the library is linked in. 
+! but deal elegantly with e.g. library wrapper routines that take arguments only used if the library is linked in.
 ! This code should be valid for any Fortran variable, is always standard conforming,
 ! and will be optimized away completely by the compiler
 #define MARK_USED(foo) IF(.FALSE.)THEN; DO ; IF(SIZE(SHAPE(foo))==-1) EXIT ;  END DO ; ENDIF
